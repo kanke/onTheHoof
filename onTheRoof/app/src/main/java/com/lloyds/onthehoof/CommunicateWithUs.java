4e38170938fed7111b98a60d68761b49c77f20eb
Angekap <Ange@Anges-MacBook-Pro.local>
2015-05-20 02:17:09 +0100

    Filled in missing functionalities
    * Merged functionality codes with templates

--- onTheRoof/app/src/main/java/com/lloyds/onthehoof/CommunicateWithUs.java
+++ onTheRoof/app/src/main/java/com/lloyds/onthehoof/CommunicateWithUs.java
@@ -1,8 +1,12 @@
 package com.lloyds.onthehoof;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,21 +34,31 @@ public class CommunicateWithUs extends Activity {
         callUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:07939694618"));
+                startActivity(callIntent);
             }
         });
 
         emailUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"example@llyodsbanking.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "your subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "message body");
+                emailIntent.setType("message/rfc822");
+                startActivity(Intent.createChooser(emailIntent, "Choose an email provider :"));
             }
         });
 
         chatWithUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.i("clicks", "You Clicked Chat Button");
+                Intent sky = new Intent(Intent.ACTION_VIEW);
+                sky.setData(Uri.parse("https://plus.google.com/hangouts/_/72cpjj252brgi4hhtqn4j68a48?hl=en"));
+                startActivity(sky);
             }
         });
 
@@ -54,6 +68,51 @@ public class CommunicateWithUs extends Activity {
 
             }
         });
+    }
+
+    //monitor phone call activities
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+                    Intent i = getBaseContext().getPackageManager()
+                            .getLaunchIntentForPackage(
+                                    getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
+        }
     }
 
     @Override
